41475d5fca1d98e015849e9c8c1d3360513bc503
oscar <omartins@ucm.es>
2025-10-22 15:03:25 +0200

    Kripke frames redo to take the property on accesibility relations out of Model.

--- Modal/modal/models/kripke.lean
+++ Modal/modal/models/kripke.lean
@@ -7,28 +7,44 @@ structure Frame where
   world : Type
   rel : world → world → Prop
 
-structure Model (Atom : Type) (h_frame : Frame → Prop) where
+namespace FramePpty
+-- Common frame properties (conditions on accessibility relations)
+-- When we need a particular type of frame, we can write
+-- def ReflexiveFrame := { f : Frame // f.reflexive }
+-- or
+-- def S4_frame (f : Frame) : Prop := f.reflexive ∧ f.transitive
+-- etc.
+def reflexive (f : Frame) : Prop :=
+  ∀ w : f.world, f.rel w w
+def symmetric (f : Frame) : Prop :=
+  ∀ w v : f.world, f.rel w v → f.rel v w
+def transitive (f : Frame) : Prop :=
+  ∀ w v u : f.world, f.rel w v → f.rel v u → f.rel w u
+def euclidean (f : Frame) : Prop :=
+  ∀ w v u : f.world, f.rel w v → f.rel w u → f.rel v u
+def serial (f : Frame) : Prop :=
+  ∀ w : f.world, ∃ v : f.world, f.rel w v
+end FramePpty
+
+structure Model (Atom : Type) where
   frame : Frame
-  h_frame : h_frame frame
   val : frame.world → Atom → Prop
 
 
 -- Defines truth at a specific world w, that is m, w ⊨ φ.
-def world_sat {Atom : Type} {h_frame : Frame → Prop}
-    (m : Model Atom h_frame) (w : m.frame.world) : Modal.Formula Atom → Prop
+def world_sat {Atom : Type} (m : Model Atom) (w : m.frame.world) :
+    Modal.Formula Atom → Prop
   | .atom a   => m.val w a
   | .bot      => False
   | .impl φ ψ => world_sat m w φ → world_sat m w ψ
   | .box φ    => ∀ v, m.frame.rel w v → world_sat m v φ
 
 -- Defines truth in an entire model m, that is m ⊨ φ.
-def model_sat {Atom : Type} {h_frame : Frame → Prop}
-    (m : Model Atom h_frame) (φ : Modal.Formula Atom) : Prop :=
+def model_sat {Atom : Type} (m : Model Atom) (φ : Modal.Formula Atom) : Prop :=
   ∀ w, world_sat m w φ
 
-def semantics {Atom : Type} {h_frame : Frame → Prop} :
-    Logic.Semantics (Formula Atom) :=
-  { model := Model Atom h_frame,
+def semantics {Atom : Type} : Logic.Semantics (Formula Atom) :=
+  { model := Model Atom,
     satisfies := model_sat }
 
 end Modal.Models.Kripke
--- Modal/modal/sound_complete/K_kripke.lean
+++ Modal/modal/sound_complete/K_kripke.lean
@@ -10,11 +10,11 @@ open Modal.ProofSystems Modal.Models
 def all_frames : Kripke.Frame → Prop := fun _ => True
 
 theorem is_sound_strong (Atom : Type) :
-    Logic.is_sound_strong (K.proof_system Atom) (@Kripke.semantics Atom all_frames) :=
+    Logic.is_sound_strong (K.proof_system Atom) Kripke.semantics :=
   by admit
 
 theorem is_complete_weak (Atom : Type) :
-     Logic.is_complete_weak (K.proof_system Atom) (@Kripke.semantics Atom all_frames) :=
+     Logic.is_complete_weak (K.proof_system Atom) Kripke.semantics :=
   by admit
 -- K is not strongly complete wrt Kripke models
 
